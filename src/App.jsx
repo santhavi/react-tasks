6e4103b2e38a535a068dc1b334ce827e3c6b6298
Santhavi S <santhavisaran06@gmail.com>
2026-07-01 09:52:09 +0530

    Added Task 2 - Name Display

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,13 @@
-//import './App.css'
-// import Header from './components/Header'
-// import Section from './components/Section'
-// import Footer from './components/Footer'
-// import Article from './components/article'
-
-// function App() {
-//   return (
-//     <>
-//       <h1>HELLO Santhavi</h1>
-
-//       <Header />
-//       <Section />
-//       <Footer/>
-//       <Article/>
-//     </>
-//   )
-// }
-
-// export default App
-
 import './App.css'
 import { useState } from 'react'
 
 function App() {
-  const [num, setNum] = useState(10);
-
-  const handleClick = ()=>{
-    // setNum(num+1)
-    // setNum(num-1)
-    setNum(0)
-  }
+  const [name, setName] = useState("");
 
   return (
     <>
-      <h1>State oda value: {num}</h1>
-      <p>{num}</p>
-      <button onClick={handleClick}>Increase</button>
-
+      <input type="text" placeholder='Enter your name' value = {name} onChange={(event) => setName(event.target.value)} />
+      <h1>Hello {name}</h1>
     </>
   );
 }
