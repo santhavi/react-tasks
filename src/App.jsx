4c2591d98d21552655c0e57dabd58f8e8eb8bd89
Santhavi S <santhavisaran06@gmail.com>
2026-07-01 10:07:55 +0530

    Added Task 2 - Name Display

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,20 @@
-import { useState } from "react";
+import './App.css'
+import { useState } from 'react'
 
 function App() {
-  const [text, setText] = useState("");
+  const [name, setName] = useState("");
 
   return (
-    <div>
-      <h2>Live Character Counter</h2>
+    <>
+      <input
+        type="text"
+        placeholder="Enter your name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
 
-      <textarea
-        rows="8"
-        cols="40"
-        value={text}
-        onChange={(e) => setText(e.target.value)}
-      ></textarea>
-
-      <h3>Characters : {text.length}</h3>
-      <h3>Words : {text === "" ? 0 : text.split(" ").length}</h3>
-    </div>
+      <h1>Hello {name}</h1>
+    </>
   );
 }
 
